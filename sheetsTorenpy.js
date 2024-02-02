d650559db5d2647813324fbfad1ee2ea798ebc5c
maita <116223644+dimikira@users.noreply.github.com>
2024-02-01 20:31:19 -0700

    Update sheetsTorenpy.js

--- sheetsTorenpy.js
+++ sheetsTorenpy.js
@@ -8,12 +8,15 @@ function onOpen() {
 function srenpy() {
   var ss = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   var data = ss.getDataRange().getValues();
+  
 
   if (data.length > 0){
     ss.getRange("E1:E").setFormula(`
     IFS(ISNONTEXT(B1) , "",
-    B1="if" , SUBSTITUTE(CONCATENATE("#---if statement\n";B1;" ";C1;"\n"),char(10),char(13)),
+    B1="if" , SUBSTITUTE(CONCATENATE("#---if statemnt\n";B1;" ";C1;"\n"),char(10),char(13)),
     B1="menu" , SUBSTITUTE(CONCATENATE("#---menu\n";B1;":\n";C1;"\n"),char(10),char(13)),
+    
+    REGEXMATCH(C1,"\n"),SUBSTITUTE(SUBSTITUTE(CONCATENATE(B1;"    """"""";C1;""""""""),"\n","\n\n"),char(10),char(13)),
 
     ISTEXT(B1) , SUBSTITUTE(SUBSTITUTE(CONCATENATE(B1;"    """;C1;""""),"\n","\n\n"),char(10),char(13))
       )`);
